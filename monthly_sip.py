3f5f64a213a2e75da6c56a6182d135c624cc583c
Sanket Karwa <sanketkarwa@Sankets-MacBook-Air.local>
2026-01-03 08:46:29 +0530

    Revert "Wire SIP engine to real data loaders"
    This reverts commit 8689930eb7a96de2fe7bf8b2dcec5d57e03f8463.

--- monthly_sip.py
+++ monthly_sip.py
@@ -3,12 +3,8 @@
 import argparse
 import datetime as dt
 import yaml
-from pathlib import Path
 
-from nifty500_universe import NIFTY500_TICKERS
-from data.fundamentals_loader import load_fundamentals_csv
 from data.mock_data import load_mock_fundamentals, load_mock_trends
-from data.prices_loader import fetch_trend_inputs, fetch_nifty_inputs
 from engine.fundamental_engine import screen_and_score, apply_sector_constraints
 from engine.trend_engine import evaluate_trends
 from engine.regime_filter import market_regime_is_cautious
@@ -24,24 +20,15 @@ def load_config(path: str) -> dict:
 def run(cfg_path: str) -> str:
     cfg = load_config(cfg_path)
 
-    data_cfg = cfg.get("data", {})
-    fundamentals_csv = Path(data_cfg.get("fundamentals_csv", "data/fundamentals.csv"))
-
-    if fundamentals_csv.exists():
-        fundamentals = load_fundamentals_csv(fundamentals_csv, NIFTY500_TICKERS)
-    else:
-        fundamentals = load_mock_fundamentals()
-
-    price_lookback = int(data_cfg.get("price_lookback_days", 260))
-    trend_inputs = fetch_trend_inputs([f.ticker for f in fundamentals], days=price_lookback)
+    fundamentals = load_mock_fundamentals()
+    trend_inputs = load_mock_trends()
 
     picks = screen_and_score(fundamentals, cfg)
     picks = apply_sector_constraints(picks, cfg)
 
-    trends = evaluate_trends(trend_inputs, cfg)
-    nifty_symbol = data_cfg.get("nifty_symbol", "^NSEI")
-    nifty_inputs = fetch_nifty_inputs(nifty_symbol, days=price_lookback)
-    regime_cautious = market_regime_is_cautious(nifty_inputs, cfg) if nifty_inputs else False
+    trends = evaluate_trends({k: v for k, v in trend_inputs.items() if k != "NIFTY50"}, cfg)
+    nifty = trend_inputs.get("NIFTY50")
+    regime_cautious = market_regime_is_cautious(nifty, cfg) if nifty else False
 
     actions = allocate_sip(picks, trends, cfg, regime_cautious)
     report = render_report(actions, regime_cautious)
